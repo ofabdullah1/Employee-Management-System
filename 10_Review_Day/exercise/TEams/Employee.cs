c1e300fdac44a5fef9d16ea1c2a5e8f054b18a75
Omar Abdullah <ofabdullah1@gmail.com>
2022-09-23 13:27:05 -0400

    first switch

---
+++ 10_Review_Day/exercise/TEams/Employee.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TEams
+{
+    class Employee
+    {
+        public long EmployeeId { get; set; }
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public double Salary { get; set; } = StartingSalary;
+        public Department Department { get; set; }
+        public string HireDate { get; set; }
+        public string FullName
+        {
+            get
+            {
+                return LastName + ", " + FirstName;
+            }
+        }
+
+
+        const double StartingSalary = 60000;
+
+
+        public Employee(long employeeId, string firstName, string lastName, string email, Department department, string hireDate)
+        {
+            EmployeeId = employeeId;
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            Department = department;
+            HireDate = hireDate;
+        }
+
+
+        public Employee() { }
+
+
+
+    }
+}
